94101839e556140d90d3770a849aa25f27cbedb8
Daniel Brian <daniel.brian@gmail.com>
2022-02-15 20:21:47 -0500

    updated readme

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,15 +1,59 @@
 import React from 'react';
+import './HornedBeast.css'
+// import Card from 'bootstrap/dist/css/bootstrap.min.css'
+import Card from 'react-bootstrap/Card'
 
 class HornedBeast extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: 0,
+    }
+  }
+
+  handleVotes = () => {
+    this.setState({
+      votes: this.state.votes + 1
+    })
+  }
+
   render(){
     return (
       <>
-        <h3>{this.props.title}</h3>
-          <img src={this.props.imageUrl} alt="The Beast!" title="a picture of the beast" />
-        <p>{this.props.description}</p>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={this.props.image_url} />
+          <Card.Body>
+            <Card.Title>{this.props.name}</Card.Title>
+            <Card.Text>
+              <button onClick={this.handleVotes}>Vote</button>
+              <table>
+                <tr>
+                  <td>Number of votes:</td>
+                  <td id="votes">{this.state.votes}</td>
+                </tr>
+              </table>
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </>
       ); //use parenthesis for multiline arguments
     }
 }
 
 export default HornedBeast;
+
+
+/*
+<div class="single-beast">
+  <img src={this.props.image_url} alt="The Beast!" title="a picture of the beast" />
+  <h2>{this.props.name}</h2>
+  <button onClick={this.handleVotes}>Vote</button>
+  <table>
+  <tr>
+    <td>Number of votes:</td>
+    <td id="votes">{this.state.votes}</td>
+  </tr>
+  </table>
+</div>
+*/
--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import HornedBeast from './HornedBeast'
+import './Main.css'
+
 
 class Main extends React.Component {
   render(){
-    return (
-      <>
-      <h2>The Beasts!</h2>
-      <HornedBeast title="The Devil" imageUrl="https://static0.srcdn.com/wordpress/wp-content/uploads/2020/01/Black-Phillip-from-The-Witch-2015.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5" description="This is the Devil"/>
 
-      <HornedBeast title="Demon Bear" imageUrl="https://cdn.inprnt.com/thumbs/37/19/3719102f984e79ba9dfc085eebff1712.jpg?response-cache-control=max-age=2628000" description="A bear, a single Bear"/>
-      </>
+    let beasts = [];
+    this.props.data.forEach(item => {
+      beasts.push(
+        <HornedBeast
+          name={item.keyword}
+          image_url={item.image_url}
+          />
+      )
+    } )
+
+    return (
+      <main>
+        <div id="beasts">
+          {beasts}
+        </div>
+      </main>
       ); //use parenthesis for multiline arguments
     }
 }
